cb4763c5bfe6b1a28b8e26994ea3a920e3f7b9d8
tanglinchuanz@163.com <tanglinchuanz@163.com>
2023-03-04 20:14:32 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# A tampermonkey script to create a split view, built for surface duo
+
+It allows you to view a page on the right side while keeping the source on the left.  
+It breaks the original behavior of links to make it possiable to navigate.  
+It also uses sandbox to counteract anti-attack behavior of some websites.  
+In all, it is a primitive and rough way that may have many unknown problems.  
+
+
+## Demo  
+![]()
+![]()
+
+## Usage  
+You need a browser that can install tampermonkey. For android, I recommend to use firefox nightly.  
+Then, copy paste from main.js.  
+
+## Known Issue 
+google.com ❌  
+stackoverflow.com ❓  
---
+++ main.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name split view
+// @namespace http://tampermonkey.net/
+// @version 0.1
+// @description originated from v2ex user @v2yllhwa
+// @author You
+// @match        https://*/*
+// @match        http://*/*
+// @match        ftp://*/*
+// @grant none
+// @run-at document-end
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    let url = window.location.href;
+    if (window.self === window.top) {
+        document.write(
+`
+<html>
+<head>
+</head>
+
+<body style="margin:0;padding:0;background:gray;" scroll="no">
+<iframe src='${url}' id='leftpage' style='width:49%;height:100%;float:left' frameborder='0' allowfullscreen='true' sandbox="allow-downloads allow-downloads-without-user-activation allow-presentation allow-modals allow-same-origin allow-scripts allow-popups allow-forms allow-pointer-lock allow-top-navigation-by-user-activation"></iframe>
+<iframe src='' id='rightpage' style='width:49%;height:100%;float:right' frameborder='0' allowfullscreen='true' sandbox="allow-downloads allow-downloads-without-user-activation allow-presentation allow-modals allow-same-origin allow-scripts allow-popups allow-forms allow-pointer-lock allow-top-navigation-by-user-activation"></iframe>
+</body>
+
+<script>
+var lefturl = '${url}';
+var righturl;
+var iframe_open_url = function (left, right) {
+if(righturl != right){
+righturl = right;
+document.getElementById('rightpage').src = righturl;
+}
+if(lefturl != left){
+lefturl = left;
+document.getElementById('leftpage').src = lefturl;
+}
+history.replaceState({},"",righturl);
+history.replaceState({},"",lefturl);
+}
+</script>
+</html>
+`
+            );
+        } else {
+            document.body.style.minWidth = "unset";
+            let nodes = document.querySelectorAll("a");
+            for (let i = 0; i < nodes.length; i++) {
+                // console.log(nodes[i].href);
+                nodes[i].addEventListener("click", function (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    window.parent.iframe_open_url(url, nodes[i].href);
+                });
+            }
+        }
+})();
